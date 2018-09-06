724613ce5674c106f9c0cb77feb0031df6d94384
Josh <grafton.josh@gmail.com>
2018-09-06 15:59:24 -0600

    Completed transition excercise. Added warning message if no selection.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // 2. If either select box does not have an option selected, surface an error
 // 3. When you click the button, make a change to the changing element
 //   (a) If 'Color' is selected, change the color of the text
@@ -8,19 +7,52 @@
 // 4. Run the transitions in order. Make sure to nest the timeouts!
 // 5. When you click the "Go!" button again, it should clear out the styles first before transition to new styles.
 document.addEventListener("DOMContentLoaded", function(event) {
-    
 
-const button = document.getElementById('transition')
-const changeElem = document.getElementById('changing')
-const firstTransition = document.getElementById('change-A')
-const secondTransition = document.getElementById('change-B')
 
-let transitionOne = ''
+  const button = document.getElementById('transition')
+  const changeElem = document.getElementById('changing')
+  const firstTransition = document.getElementById('change-A')
+  const secondTransition = document.getElementById('change-B')
 
-// 1. Add an event listener to the button on click
-button.addEventListener('click', function () {
-  let transitionOne = document.getElementById('change-A').value
-  changeElem.style.transitionOne = 'blue'
-})
 
+
+  // 1. Add an event listener to the button on click
+  button.addEventListener('click', function() {
+    let alert = document.querySelector('.alert.alert-warning')
+    if (alert) {
+        error.removeChild(alert)
+    }
+    if(firstTransition.selectedIndex === 0 || secondTransition.selectedIndex === 0) {
+      let errorMessage = document.createElement('div')
+      errorMessage.setAttribute('class', 'alert alert-warning')
+      errorMessage.innerText = 'Warning!'
+      document.getElementById('error').appendChild(errorMessage)
+      return
+    }
+    setTimeout(function() {
+      if (firstTransition.selectedIndex === 1) {
+        changeElem.style.color = 'green'
+      } else if (firstTransition.selectedIndex === 2) {
+        changeElem.style.backgroundColor = 'purple'
+      } else if (firstTransition.selectedIndex === 3) {
+        changeElem.style.fontStyle = 'oblique'
+      } else if (firstTransition.selectedIndex === 4) {
+        changeElem.style.fontWeight = 'bold'
+      }
+    }, 1000)
+    setTimeout(function() {
+      if (secondTransition.selectedIndex === 1) {
+        changeElem.style.color = 'green'
+      } else if (secondTransition.selectedIndex === 2) {
+        changeElem.style.backgroundColor = 'purple'
+      } else if (secondTransition.selectedIndex === 3) {
+        changeElem.style.fontStyle = 'oblique'
+      } else if (secondTransition.selectedIndex === 4) {
+        changeElem.style.fontWeight = 'bold'
+      }
+    }, 5000)
+    button.addEventListener('click', function() {
+      changeElem.style = 'inherit'
+    })
+  })
 })
